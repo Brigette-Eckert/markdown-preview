abbd08003e19820bf2a482709f0a0a230eb9a179
Brigette Eckert <brigette.eckert@live.com>
2016-09-28 00:44:57 -0700

    remove hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader?presets[]=es2015&presets[]=react&presets[]=react-hmre"},
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader?presets[]=es2015&presets[]=react"},
             {test: /\.scss$/, loaders: ["style", "css", "sass"]}
         ]
     },
